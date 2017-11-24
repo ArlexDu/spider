5b00bf51a9b87fb26c93ddb0c8cf7e4159c6c786
杜盛瑀 <772773671@qq.com>
2017-11-24 17:54:32 +0800

    百度图片爬虫测试成功

--- CatchPicFromBaidu.py
+++ CatchPicFromBaidu.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import time
-import urllib
 import requests
 
 # print(requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS)
@@ -8,13 +7,12 @@ url="http://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&c
 
 xpath='//li[contains(@class,"imgitem")]/div/a/img'
 
-driver = webdriver.Chrome(executable_path=r"./webDriver/chromedriver.exe")
+driver = webdriver.Chrome(executable_path=r"./webDriver/chromedriver")
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
 # driver.maximize_window()
 
-header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
-        'Referer':'https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&fm=index&pos=history&word=%E9%A9%AC%E5%85%8B%E6%9D%AF'}
+header={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
 
 img_url_array=[]
 
@@ -23,23 +21,22 @@ driver.get(url)
 pos=0
 m=0
 
-for i in range(1):
+for i in range(10):
     pos+=i*500
     js="document.documentElement.scrollTop=%d" % pos
     driver.execute_script(js)
     time.sleep(1)
 
     for element in driver.find_elements_by_xpath(xpath):
-        img_url = element.get_attribute('src')
+        img_url = element.get_attribute('data-imgurl')
         if img_url != None and img_url not in img_url_array:
             img_url_array.append(img_url)
             m+=1
             type = img_url.split('.')[-1]
             print("download {}".format(img_url))
             filename=str(m)+'.'+type
-            data=requests.get(img_url).text
-            print('data {0}'.format(data))
+            data=requests.get(img_url,headers=header).content
             f=open('./baiduimages/'+filename,'wb')
-            f.write(data.encode())
+            f.write(data)
             f.close()
 driver.close()
